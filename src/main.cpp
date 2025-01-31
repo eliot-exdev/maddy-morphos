409271b2cd7b4c91e3aa9337f5144999d22077b3
Andre Geisler <andre@exdev.de>
2025-01-31 11:59:26 +0100

    some minor optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -19,17 +19,18 @@ int main(int argc, char **argv) {
     }
 
     const std::filesystem::path inputFile{argv[1]};
-    if (!std::filesystem::exists(inputFile) || !std::filesystem::is_regular_file(inputFile)) {
-        std::cout << "could not open file: " << argv[1] << std::endl;
+    if (!std::filesystem::exists(inputFile) || !std::filesystem::is_regular_file(inputFile) || inputFile.extension() != ".md") {
+        std::cout << "could not open file: " << inputFile.string() << std::endl;
         return 2;
     }
 
     const std::filesystem::path outputFile{argv[2]};
     if (std::filesystem::exists(outputFile)) {
-        std::cout << "file already exists: : " << argv[2] << std::endl;
+        std::cout << "file already exists: : " << outputFile.string() << std::endl;
         return 3;
     }
 
+    // open input stream
     std::fstream markdownInput{inputFile.string(), std::fstream::in};
 
     // config is optional
@@ -37,9 +38,12 @@ int main(int argc, char **argv) {
     config->enabledParsers &= ~maddy::types::EMPHASIZED_PARSER;// equivalent to !isEmphasizedParserEnabled
     config->enabledParsers |= maddy::types::HTML_PARSER;       // equivalent to !isHTMLWrappedInParagraph
 
-    auto parser = std::make_shared<maddy::Parser>(config);
+    // parse
+    auto parser = std::make_unique<maddy::Parser>(std::move(config));
     const auto html = parser->Parse(markdownInput);
+    parser.reset();
 
+    // write output
     std::fstream htmlOutput{outputFile.string(), std::fstream::out};
     htmlOutput.write(html.c_str(), html.length());
 
